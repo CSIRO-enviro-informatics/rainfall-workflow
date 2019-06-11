8152ea59ff4509faf9bb81e2e3f8196008afc7a6
annaandraszek <annareach2@gmail.com>
2019-06-11 14:37:27 +1000

    Wrote description of workflow steps, will be using dojobber

---
+++ workflow.py
@@ -0,0 +1,32 @@
+# Inputs:
+# 	- SMIPS rainfall 1km grid - predictand (DV) - source: CSIRO
+# 	- ACCESS-G or -R 12 km grid - predictor (IV) - source: NCI
+# 		○ Re-gridded to 1 km grid
+#
+# Jobs:
+# 	- Re-grid ACCESS data from 12km grid to 1km
+# 	- For each grid point: Create post-processed forecast
+# 		○ Extract symmetric grid point(s) from predictand and predictor
+# 		○ Model fit/forecast
+# 			§ Transform predictor and predictand time series to normal distributions
+# 			§ RPP-SC - call fit() during training , and forecast() during regular use
+# 				□ Model parameters from fit() are saved and loaded to forecast()
+# 			§ Output: post-processed 7 day time series forecast for 1km grid point.
+# 				□ Dimensions: time (1), lead time (7), ensemble member (1000)
+# 	- Reassemble grid
+# 	- For each grid point: restore spatial correlations
+# 		○ Shuffle - restore spatial correlations
+# 			§ Output: shuffled 7 day forecast for 1km grid point
+# 	- Hydrological model
+# 		○ Output: 7 day ensemble soil moisture forecast (API)
+
+import argparse
+import logging
+import os
+import sys
+
+import dojobber
+from dojobber import Job
+from dojobber import DummyJob
+from dojobber import RunonlyJob
+
