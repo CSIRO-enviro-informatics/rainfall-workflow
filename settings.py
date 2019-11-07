1be11d8fc8bb183094cbd4236af3ac6acbf9ef1c
annaandraszek <annareach2@gmail.com>
2019-11-07 11:48:30 +1100

    Making sure the date is a string when giving it to a filename, and making it one if it's not

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import platform
 import datetime
+from dates import date2str
 
 if 'Linux' in platform.platform():
     ACCESS_G_PATH = '/OSM/CBR/LW_SATSOILMOIST/source/BOM-ACCESS-G/ACCESS_G_12z/'  # access-g write path
@@ -21,7 +22,7 @@ PARAMS_AGG = PARAMS_PATH + 'PARAMS_aggregated.nc'
 
 FORECAST_PATH = TEST_PATH + 'forecast/'
 FORECAST_GRID_PATH = FORECAST_PATH + 'grids/'
-FORECAST_AGG = FORECAST_PATH + 'FORECAST_aggregated.nc'
+#FORECAST_AGG = FORECAST_PATH + 'FORECAST_aggregated.nc'
 
 SMIPS_CONTAINER = 'SMIPSv0.5.nc'  # name of container in SMIPS_PATH
 
@@ -35,17 +36,38 @@ yesterday = datetime.date.today() - datetime.timedelta(days=1)
 
 ACCESS_G_NUM_LEAD_DAYS = 10
 
+
 # combine with ACCESS_G_PATH for a specific file path
 def access_g_filename(str_date):
+    str_date = date_type_check(str_date)
     return str_date[:4] + '/ACCESS_G_accum_prcp_fc_' + str_date + ACCESS_HR + '.nc'
 
 # combine with SMIPS_DEST_PATH for a specific file path
 def smips_filename(str_date):
+    str_date = date_type_check(str_date)
     return str_date[:4] + '/SMIPS_blnd_prcp_regrid_'+ str_date +'.nc'
+
 
 # combine with PARAMS_GRIDS_PATH for a specific file path
 def params_filename(lat, lon):
     return 'params_' + str(lat) +'_' + str(lon) + '.nc'
 
+
 def forecast_filename(str_date, lat, lon):
+    str_date = date_type_check(str_date)
     return 'forecast_'+ str_date + '_' + str(lat) +'_' + str(lon) + '.nc'
+
+
+def forecast_agg(str_date):
+    str_date = date_type_check(str_date)
+    return FORECAST_PATH + str_date + '_FORECAST_aggregated.nc'
+
+
+def date_type_check(date):
+    if type(date) == 'str':
+        return date
+    else:
+        if type(date) == 'datetime.date' or type(date) == 'datetime.datetime':
+            return date2str(date)
+        else:
+            raise TypeError('Date is of the wrong type: ' + type(date))
