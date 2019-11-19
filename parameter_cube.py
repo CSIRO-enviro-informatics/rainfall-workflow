d8fba375198225cb3731f65740b8c8539ed83858
annaandraszek <annareach2@gmail.com>
2019-11-19 12:18:41 +1100

    Undoing/improving change made in last commit of these files

--- parameter_cube.py
+++ parameter_cube.py
@@ -190,8 +190,7 @@ def add_to_netcdf_cube_from_files(files, cubename):
 
     for file2process in files:
         file = file2process
-        file_only = file.split('/')[-1] # new network path has underscores in directories
-        _, lat, lon = file_only.rstrip('.nc').split('_')
+        lat, lon = file.rstrip('.nc').split('_')[-2:]
         lat, lon = round(float(lat), 2), round(float(lon), 2)  # rounding lat and lon values to sacrifice accuracy for consistency in the dictionary lookup
 
         dataset = xr.open_dataset(file, decode_times=False)
